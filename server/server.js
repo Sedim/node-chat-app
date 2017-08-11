ea32cacb3055fd6d4dcd97f9b2c73695b108a13c
Sedim Al-Araji <sedim@launchit.com>
2017-08-11 19:56:15 -0400

    Added function generateMessage (from, text)

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require ('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public'); //This eleminates the ../ path and makes it a clean path
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,32 +15,28 @@ app.use(express.static(publicPath));
 
 //Register an event listener
 io.on('connection', (socket) => {
+
   console.log('new user connected')
+
+// Use socket.emit  to emit only the emmiting client :
+//socket.emit('newMessage', Server ==> to the emmiting client
+
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat room'));
+
+// Use socket.broadcast.emit to emit to all clients except the original emmiter:
+//socket.broadcast.emit('newMessage', Server ==> ALL Clients except emmiting client
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined the chat room'));
+
   //****>>type at the consol dev tool of browser(client) : socket.emit('createMessage', {from: 'julie', text: 'hi there'});
   socket.on('createMessage', (newMessage)=> {// Server <== client
     console.log('createMessage', newMessage);
-    // Use socket.emit  to respond only to the emmiting client :
-    //socket.emit('newMessage', {// Server ==> to the emmiting client
-    // Use socket.broadcast.emit emit to all clients except the emmiter:
-    //socket.broadcast.emit('newMessage', {// Server ==> Client
-    //Use io.emit to broadcast to ALL clients including the sending one:
-    io.emit('newMessage', {// Server ==> all Clients
-    from: newMessage.from,
-    text: newMessage.text,
-    createdAt: new Date().getTime()
-    });
-
-
-
-});
-
-
-
+    //Use io.emit to broadcast to ALL clients including the response to the emmiting client: Server ==> all Clients
+    io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+  });
 
   socket.on('disconnect', function (){
     console.log('disconnected from client');
   });
-
 
 });
 
