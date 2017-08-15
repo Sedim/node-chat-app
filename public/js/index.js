dac483f16c3acf936ea5d2f6340e57c084f1cbf4
Sedim Al-Araji <sedim@launchit.com>
2017-08-15 10:52:53 -0400

    Added css and formatting

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,14 @@
 //Initial
-var socket = io();//Insatance of io sockets
+var socket = io(); //Insatance of io sockets
 //Standard socket calls
-socket.on('connect', function () {//'connect' is built in
+socket.on('connect', function() { //'connect' is built in
   console.log('Connected to server');
 });
 
-socket.on('disconnect', function() {//'disconnect is built in'
+socket.on('disconnect', function() { //'disconnect is built in'
   console.log('Disconnected from server');
 });
+
 
 //you can type at the consol dev tool of browser : socket.emit('createMessage', {from: 'julie', text: 'hi there'});
 
@@ -15,15 +16,15 @@ socket.on('disconnect', function() {//'disconnect is built in'
 //**************** Receiving message from server:
 socket.on('newMessage', function(message) {
   console.log('New Message:', message);
-  var dd = jQuery('<dd></dd>');// set up an unordered list tag
+  var dd = jQuery('<dd></dd>'); // set up an unordered list tag
   dd.text(`==>${message.from}: ${message.text}`); //Add the data to it
 
-  jQuery('#messagelist').append(dd);// append it to the html and Display it
+  jQuery('#messagelist').append(dd); // append it to the html and Display it
 });
- 
-socket.on('newLocationMessage', function(message){
+
+socket.on('newLocationMessage', function(message) {
   console.log('Location:', message);
-  var dd = jQuery('<dd></dd>');// set up an unordered list tag
+  var dd = jQuery('<dd></dd>'); // set up an unordered list tag
   //Set up the Anchor
   var a = jQuery('<a target="_blank">Click here for my current location</a>');
   //The reason we are setting the stuff below instead of just adding it through a mask
@@ -31,47 +32,61 @@ socket.on('newLocationMessage', function(message){
   dd.text(`${message.from}: `); //construct the DOM
   a.attr('href', message.url); // Construct the DOM
   dd.append(a);
-  console.log('Compsed DOM:', dd)
+  console.log('Compsed DOM:', dd);
   jQuery('#messagelist').append(dd);
 });
 
 // socket.emit('createMessage', {
 //   from: 'frank',
 //   text: 'Hi'
-// }, function (data) {
+// }, function(data) {
 //   console.log('Got it', data);
 // });
 
-//**************** CLIENT(createMessage) ==> SERVER(createMessage)
+//**************** CLIENT(createMessage) ==> SERVER(createMessage) //
 //**************** emiting a message to the server
 // Disable the standard FORM behaviour and on click:
+
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
-//on click:
+  /**
+   * [messageTextbox: Created to quickly change the messaage area]
+   * @type {[Object]}
+   */
+  var messageTextbox = jQuery('[name=message]');
+
+  //on click:
   socket.emit('createMessage', {
-      from: 'User',
-      text: jQuery('[name=message]').val() // Send the actual text
-   }, function (){ // Acknowledge receipt from the server function
+    from: 'User',
+    text: messageTextbox.val() // Send the actual text
+  }, function() { // Acknowledge receipt from the server function
+    messageTextbox.val(''); // Clear the message form field after submision
   });
 });
+
 
 //**************** CLIENT(createLocationMessage) ==> SERVER(createLocationMessage)
 //**************** Pressing and sending location info to the server
 var locationButton = jQuery('#send-location');
-
-locationButton.on('click', function () {
-console.log('Click');
-  if (!navigator.geolocation) {// if there is no built in navigator in your broswer
-    return alert('Geolocation not supported by you browser.')
+locationButton.on('click', function() {
+  console.log('Click');
+  if (!navigator.geolocation) { // if there is no built in navigator in your broswer
+    return alert('Geolocation not supported by you browser.');
   }
 
-  navigator.geolocation.getCurrentPosition(function (position) {
+  //the disabled attribute is set to dissabled and disables the button
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location'); // Re-enables the button
     console.log(position);
-    socket.emit('createLocationMessage', {// client ==> server
-      latitude: position.coords.latitude,// see the DOM
+    socket.emit('createLocationMessage', { // client ==> server
+      latitude: position.coords.latitude, // Nvigate the DOM
       longitude: position.coords.longitude
     });
-  }, function () {
+  }, function() {
+    locationButton.removeAttr('disabled').text('Send location'); // Re-enables the button
     alert('Unable to fetch location.');
   });
 });
